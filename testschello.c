a0401587d8748b7967783e5ac4376db5b9678f85
liuyunfei <seliuyunfei@163.com>
2017-11-08 03:03:20 -0800

    It's a test!

---
+++ testschello.c
@@ -0,0 +1,11 @@
+#include<unistd.h>
+#include<sys/syscall.h>
+#include<sys/types.h>
+#include<stdio.h>
+#define __NR_schello 385
+int main(int argc,char *argv[])
+{
+syscall(__NR_schello);
+printf("ok!run dmesg|grep hello in terminal!\n");
+return 0;
+}
